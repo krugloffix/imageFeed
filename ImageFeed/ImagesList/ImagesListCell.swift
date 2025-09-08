d5363798989aa892d69fb54337781bc4dfebd499
krugloffix <krugloffix@gmail.com>
2025-09-08 22:38:43 +0700

    homework 8 finished

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+}
